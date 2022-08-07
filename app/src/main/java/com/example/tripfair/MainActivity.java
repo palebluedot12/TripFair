99e59674471cf49181bb3ad3c3df3e1640411955
vpdzjs <vpdzjs@gmail.com>
2022-08-08 01:43:14 +0900

    2022-08-08 구글 넣기 전 로그인

--- app/src/main/java/com/example/tripfair/MainActivity.java
+++ app/src/main/java/com/example/tripfair/MainActivity.java
@@ -1,62 +1,14 @@
 package com.example.tripfair;
 
-import static android.content.ContentValues.TAG;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
-import android.view.MenuItem;
-
-import com.google.android.material.navigation.NavigationBarView;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
-
-    HomeFragment homeFragment;
-    MapFragment mapFragment;
-    MypageFragment mypageFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        homeFragment = new HomeFragment();
-        mapFragment = new MapFragment();
-        mypageFragment = new MypageFragment();
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.containers, homeFragment).commit();
-
-        NavigationBarView navigationBarView = findViewById(R.id.bottom_navigationview);
-        navigationBarView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.home:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, homeFragment).commit();
-                        return true;
-                    case R.id.map:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, mapFragment).commit();
-                        return true;
-                    case R.id.mypage:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, mypageFragment).commit();
-                        return true;
-                }
-                return false;
-            }
-        });
-
-
     }
-
-
 }
